420317ece720d96b198a7546330676ab4f3c6d0a
Danny <leodanny024@gmail.com>
2025-07-27 03:00:54 -0600

    Configuration.

---
+++ src/app/features/auth/features/auth-log-in/auth-log-in.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-log-in',
+  imports: [],
+  templateUrl: './auth-log-in.html',
+  styleUrl: './auth-log-in.css'
+})
+export class AuthLogIn {
+
+}
